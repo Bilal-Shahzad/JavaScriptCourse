484d63d669e1b77ab9186931c9989d2ff48f3de3
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-09-22 13:29:37 -0500

    variables and lab

---
+++ variables.js
@@ -0,0 +1,17 @@
+// invalid
+// invalid variables begin with a number
+// contain a javascript keyword 
+// contain a space between words 
+
+// valid
+// names must begin with a letter or $ or _
+// starts with letters
+// camelCase 
+// first word is lowercase and the remaining words are capitalized with the first letter
+
+var Jordan = 23
+// Equal sign is a assignment operator 
+// used to update a value given to a variable 
+
+
+
---
+++ variableslab.js
@@ -0,0 +1,19 @@
+var example = 'first example';
+
+// change variables to valid and best practice 
+
+// var 1stPet = 'Bear;
+var firstPet = 'Bear';
+var pet1 = 'Bear'
+
+// var BaseballTeam = 'Dodgers'
+var baseballTeam = 'Dodgers';
+ 
+// var favoritedayoftheweek = 'Friday';
+var favoriteDayOfTheWeek = 'Friday';
+
+// var favorite color = 'green';
+var favoriteColor = 'green';
+
+// var var = 'car';
+var car = 'Honda'; 
