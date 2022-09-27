b1027f78a5acec4565957847d7e47ac10624d7f8
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-09-26 19:08:58 -0500

    strings javascript

---
+++ buildingblocks/strings.js
@@ -0,0 +1,30 @@
+// strings
+// data type used in to represent text and is in either in single or double quotes
+
+
+// primitive data types
+//string, number, boolean, null, udefined, symbol
+
+// best practice
+//choose one approach and be consistent 
+var name = 'Bilal';
+var name = "Bilal";
+// make sure you dont mix them 
+
+
+//EX
+// var greeting = 'It's great to see you!'; //error
+var greeting = "It's great to see you!";
+
+// var response = "Billy said, "I am sick""; //error
+var response = 'Billy said, "I am sick"';
+
+// Escape character
+//backslash excape character turns special characters into string characters
+var greeting = 'It\'s great to see you!';
+var response = "Billy said, \"I am sick""; 
+
+//Length Property 
+// Returns lentgh of a string 
+cvar greeting = "It's goot to meet you!";
+greeting.length; //22
