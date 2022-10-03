ef74f23fdd35c0fd459c63105d64b293f23262e6
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-10-03 18:28:24 -0500

    operators video (not finishedd

---
+++ buildingblocks/operators.js
@@ -0,0 +1,48 @@
+// Javascript operators are used to assign values, to copmpae values, perform arithmetic operations 
+// Operators allows to create a single value from one or more values 
+
+
+// Binary operators requires two operands one before and one after the operator 
+// 2 + 3
+//two and three are operands while the (+) is the operator
+
+//Unary operators requires a single operand either before or after the operator
+//y++ 
+// y is the operand and (+) is the operator
+
+// operaotrs take the number value either literals or variables as their operations and returns a single number value 
+
+var count = 10 + 5 - 4;
+console.log(count)
+
+var mult = 3*5 ;
+console.log(mult)
+
+var div = 12/4;
+console.log(div)
+
+var remainder = 21 % 4
+console.log(remainder)
+//gives us the remainder of 21 and 4 
+
+// x = 14
+// var count = 5
+// count + = 4;
+// line 30 is adding 4 to 5 
+// count = count - 5;
+// count - = 5;
+// line 33 is subtracting 5 from var count = 5 
+
+// double equals ==
+// double equals whether two operands are equal 
+
+// triple equals ===
+//strict quality operand and checks using a strict definition of equality 
+
+// Both checks for equality betwwen two values or operands 
+
+var x = 8;
+var y = '8';
+var equals = x == y;
+console.log("hittin here", equals)
+//if we want to see if they are equal use the double equal signs 
