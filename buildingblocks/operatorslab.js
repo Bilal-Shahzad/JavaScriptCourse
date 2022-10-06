17076ec73a0175a15cd715df7240672be66da535
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-10-05 23:18:50 -0500

    opertators lab

---
+++ buildingblocks/operatorslab.js
@@ -0,0 +1,75 @@
+// complete the folowing with arithmitic operators
+
+// var addition = ;
+var addition = 5 + 2;
+console.log(addition)
+
+// var subtraction = ;
+var subtraction = 10 -2;
+console.log(subtraction)
+
+// var mult = ;
+var mult = 10 * 10;
+console.log(mult)
+
+// var div = ;
+var div = 9 / 9;
+console.log(div)
+
+// var modulus = ;
+var modulus = 41 % 7;
+console.log(modulus)
+
+
+//assignment operators 
+
+//shorten examples
+
+//var x = 20;
+
+// x = x + 5;
+x +=5 ;
+//x = x - 5;
+x -=5 ;
+// x = x * 5;
+x *=5 ;
+// x = x % 5;
+x %=5;
+
+// double and triple equals 
+// make all results true 
+
+// var x = 12;
+var x = 12;
+
+// var y = '12';
+var y = '12';
+
+// var z = 14;
+var z = 14;
+
+var equals = x == y;
+console.log(equals)
+
+var equals2 = x !== y;
+console.log(equals2)
+// ! is the inequality operator 
+// we do a double equal and it will evaluate to true we do a double equal because x and y have the same values but they are different types.
+
+
+
+//concentration operator 
+//create happyGreetiing
+// create angryGreeting
+
+var greeting = 'Hello';
+
+var angryAtGuest = 'get out of my house!';
+
+var happyWithMyGuest = 'thanks for coming over';
+
+var happyGreeting = greeting + '' + happyWithMyGuest
+console.log(happyGreeting)
+
+var angryGreeting = greeting + ' ' + angryAtGuest
+console.log(angryGreeting)
