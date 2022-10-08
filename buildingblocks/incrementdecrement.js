1e97ded0115ce0249a22edeebd0f81b2e6ba4729
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-10-08 00:02:37 -0500

    increment decrement values

---
+++ buildingblocks/incrementdecrement.js
@@ -0,0 +1,38 @@
+ //increment operator 
+ //adds one to its operand and returns a value 
+
+ //syntax 
+ //operator : x++ or ++x
+ 
+
+ //ex 
+ var x = 5;
+
+ //x returns 5 
+
+ var bilal = x++;
+ console.log(bilal);
+//5
+ var bilal2 = x;
+ console.log(bilal2)
+ //6
+
+ //value is incremented by one after original value first returned
+
+ var bilal3 = ++x
+ console.log(bilal3)
+ //7
+
+
+ //decrement operator subtracts from its operand and returns a value 
+// syntax 
+//operator: x-- or --x
+
+
+//decrement operator to immi=ediately return lower value 
+//use decrement operator to first return original value then lower the value 
+
+var y = 7;
+
+var bilal5 = --y;
+conosole.log(bilal5)
