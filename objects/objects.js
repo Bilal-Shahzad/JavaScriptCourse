b48a242688c41f4dddd74a2c1c2a44627f6f4ec6
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-10-13 22:41:34 -0500

    objects lab

---
+++ objects/objects.js
@@ -0,0 +1,38 @@
+//object is a value that has key/value pairs
+//anything that isnt primitive (string,boolean,null,number,undefined,symbol) is an object
+
+// if a variable is apart of a object it is called a property 
+// functions are known as methods, they represent the taks associated with them
+
+//object literal notation {}
+
+var car = {
+    wheels: 4,
+    color: 'silver',
+    mpg: 34,
+    engine: 1,
+    gallons: 12
+};
+//you have commas except fo the ending key value pair 
+
+// dot noation
+
+var vehichle = {};
+object.property = value;
+car.wheels = 4;
+car.color = 'red';
+car.pg = 34;
+car.engine = 1;
+car.gallons = 12;
+console.log(vehichle);
+
+// bracket notation
+
+var car2 = {};
+object['property'] = value;
+car['wheels']= 4;
+car ['color'] = 'red';
+car['mpg']= 34;
+car['engine']=1;
+car['gallons']=12;
+console.log(car2)
---
+++ objects/objectslab.js
@@ -0,0 +1,32 @@
+// build a person with object literal notation 
+//use three properties values constisting of string number and boolean
+
+var person = {
+    arms: 2,
+    age: '20',
+    blackHair: true
+}
+
+console.log(person)
+
+//build an onject literal notation
+//use dot notaion to add four propertites and values
+//make pizza
+
+var pizza = {}
+    pizza.sauce = 'marinara';
+    pizza.topping = 'cheese';
+    pizza.toppings = 'mushrroms';
+    pizza.numberOfMushrooms = 4
+console.log(pizza)
+
+//bracket notation 
+//use boolean string and number 
+// build a cup
+
+var cup = {};
+cup ['plastic'] = true;
+cup ['liquid'] = 'water'
+cup ['weight'] = '5lbs'
+
+console.log(cup)
