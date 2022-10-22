a06a2d62eef8bbc47e66f1fb745aba2dad741680
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-10-22 00:05:53 -0500

    lab

---
+++ objects/DB_lab.js
@@ -0,0 +1,13 @@
+var snowman = {
+    'isSnow': true,
+    'color': "white", 
+    'madeOfSnowballs': 3,
+    'first name': "Frosty"
+}
+console.log(snowman)
+//access properties 
+snowman ['isnow'] = false
+snowman. color = 'yellow'
+snowman ['madeOfSnowballs'] = 1
+snowman ['first name'] = 'Bilal'
+console.log(snowman)
