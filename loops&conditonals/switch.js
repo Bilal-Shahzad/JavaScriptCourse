113e04e95abab7be8506f6398007c55bcac04e2f
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-11-29 17:40:44 -0600

    practice

--- loops&conditonals/switch.js
+++ loops&conditonals/switch.js
@@ -1,15 +1,15 @@
 //switch statemnets is used to evaluate an expression then associating it with a case clause and finally running code executing statements matching that case
 
-// switch (expression) {
-//     case value:
-//         code block;
-//         break;
-// case y;
-//     code block;
-//     break;
-//     default:
-//         code block;
-// }
+switch (expression) {
+    case value:
+        code block;
+        break;
+case y;
+    code block;
+    break;
+    default:
+        code block;
+}
 
 var grade = 'B';
 switch (grade) {
@@ -17,7 +17,7 @@ switch (grade) {
         alert('Great Job');
         break;
     case 'B':
-        alert('Goof Job');
+        alert('Good Job');
         break;
     case 'C':
         alert('Good Try');
@@ -28,3 +28,20 @@ switch (grade) {
 console.log(grade)
 //the switch expression is evaluated once 
 //the value of the expression is compared with the values of each case
+
+//extra practice
+var fruits = 'mango';
+switch (fruits) {
+  case 'orange':
+    console.log('Oranges are $02 a pound.');
+    break;
+  case 'mango':
+  case 'pears':
+    console.log('mangoes and pears are $5.00 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+      console.log('We have fruits(fruits).')
+}
+// expression
+// An expression whose result is matched against each case clause.
