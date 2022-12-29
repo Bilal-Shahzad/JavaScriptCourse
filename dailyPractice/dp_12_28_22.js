2f4a8e1ec426573e302a601b537d7dff08ce93e5
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-12-28 23:57:48 -0600

    -

---
+++ dailyPractice/dp_12_28_22.js
@@ -0,0 +1,48 @@
+function randomNumbers(n) {
+    randomArray - []
+
+    for (let i=0; i <=n; i++) {
+        randomArray.push(Math.random())
+    }
+    return randomArray;
+}
+
+randomArray = randomNumbers(8);
+console.log(n(8));
+console.log(randomNumbers.length);
+randomArray.push(8);
+console.log(randomArray);
+number.unshift(2);
+console.log(randomNumbers);
+
+var dictionary = {
+    person1 : {
+     firstName: "Bilal",
+     lastName: 'Shahzad',
+     age: 20,
+    },
+    person2 : {
+        firstName: "Lightning",
+        lastName: "McQueen",
+        age: 50,
+    },
+    person3 : {
+        firstName: "Saquon",
+        lastName: "Barklet",
+        age: 24,
+    },
+    person4 : {
+        firstName: "J",
+        lastName : "Jettas",
+        age: 23,
+    },
+    person5 : {
+     firstName: "Elmer",
+     lastName: "Fudd",
+     age: 100,
+    }
+ }
+ console.log(dictionary)
+ 
+ dictionary.person3.age=10
+ console.log(dictionary)
