7be4c78f39b1e92e11f1370487101a1835e80d01
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-12-29 15:12:17 -0600

    practice

---
+++ dailyPractice/dp_12_29_22.js
@@ -0,0 +1,53 @@
+function randomNumbers(n) {
+    randomArray = []
+    for (let i=0; i <=n; i++) {
+        randomArray.push(Math.random())
+    }
+    return randomArray;
+}
+randomArray = randomNumbers(8);
+console.log(n(8));
+console.log(randomNumbers.length);
+randomArray.push(8);
+console.log(randomArray);
+number.unshift(2);
+console.log(randomNumbers);
+
+var dictionary = {
+    person1 : {
+     firstName: "Bilal",
+     lastName: 'Shahzad',
+     age: 20,
+    },
+    person2 : {
+        firstName: "Lightning",
+        lastName: "McQueen",
+        age: 50,
+    },
+    person3 : {
+        firstName: "Doc",
+        lastName: "Hudson",
+        age: 29,
+    },
+    person4 : {
+        firstName: "Ben",
+        lastName : "Dover",
+        age: 35,
+    },
+    person5 : {
+     firstName: "Buggs",
+     lastName: "Bunny",
+     age: 22,
+    }
+    
+ }
+ console.log(dictionary)
+ dictionary.person3.age=12
+ console.log(dictionary)
+
+ dictionary.person1.weight = 170
+ dictionary.person2.weight = 220
+ dictionary.person3.weight = 110
+ dictionary.person4.weight = 180
+ dictionary.person5.weight = 190
+ console.log(dictionary)
