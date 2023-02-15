792b6ab90ebbebbc4552f4e8fadc38b37e922a18
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-02-14 20:26:27 -0600

    for each

---
+++ ES6/forEach.js
@@ -0,0 +1,28 @@
+// forEach()
+// calls a provided callback function once for each element in an array in ascending order
+
+
+// syntax
+
+// array.forEach(function(currentValue, index, array) thisValue)
+// in order 
+
+// array is the array 
+// for.Each is the keyword 
+// function is the call back function 
+// currentValue is the current value iterating through 
+// index (optional)
+// array object (optional)
+// thisValue (optional)
+
+// forLoop 
+var scaryCreatures = ['lions', 'tigers', 'bears']
+for( var i = 0; i < scaryCreatures.length; i++) {
+    console.log(`Dorothy is afraid of ${scaryCreaturs[i]}.`))
+}
+
+// forEach
+scaryCreatures.forEach(function(scaryCreatures) {
+    console.log(`Dorothy is afraid of ${scaryCreatures})`)
+    
+})
