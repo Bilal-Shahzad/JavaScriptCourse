912b37481d3ad008c5e28a5adcb04c5c4e14e3a1
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-02-16 22:31:27 -0600

    practice

---
+++ dailyPractice/2_16_23.js
@@ -0,0 +1,35 @@
+function newArray(n) {
+    numbers = []
+    for (var i = 0; i <= n; i++) {
+        numbers.push(Math.random())
+    }
+    return numbers
+}
+
+var set = newArray(8)
+console.log(set)
+set.push(8)
+console.log(set)
+set.unshift(2)
+console.log(set)
+set.unshift(2)
+console.log(set)
+var newSet = new Set(set)
+console.log(newSet)
+
+var people = {
+    'Blue': 11,
+    'Mac': 11,
+    'Harringman': 70,
+    'Wilt': 60,
+    'Eduardo': 43
+}
+console.log(people)
+var namesOfPeople = Object.keys(people)
+console.log(names)
+var agesOfPeople =  Object.values(people)
+console.log(age)
+people.Mac = 13
+console.log(people)
+var deleteAge = delete people.Mac['13'];
+console.log(deleteAge)
