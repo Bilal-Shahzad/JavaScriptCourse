edf8ffe9707c376e0a47c61d344d09bb1e9154ee
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-03-17 23:11:33 -0500

    practice

---
+++ dailyPractice/3_17_23.js
@@ -0,0 +1,11 @@
+function findLargestNumber(arr) {
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+    }
+    return largest;
+  }
+  console.log(findMax([100,300,400,600]))
+  
