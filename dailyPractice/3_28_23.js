f6bb74b529c0127c3d542fa61cdf2d3371ca5265
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-03-28 18:48:30 -0500

    practice

---
+++ dailyPractice/3_28_23.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#my-button');
+
+const div = document.getElementById('my-div');
+
+const paragraphs = document.querySelectorAll('.my-paragraph');
+
+const paragraphs = document.querySelectorAll('.my-paragraph');
+
+const inputs = document.getElementsByName('my-input');
