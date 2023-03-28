3b405ec725c61cce3d98e79f4895550067372311
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-03-27 19:34:43 -0500

    todo list prep

---
+++ .vscode/toDoApp.js/toDo.js
@@ -0,0 +1,21 @@
+var form = document.query('form')
+var todoList = document.querySelector('ul')
+var button = document.querySelector('button')
+var input = document.getElementById('user-todo')
+
+form.addEventListner('submit' , function(e) {
+    e.preventDefault()
+    todoMaker(input,value)
+    input.value = ' '
+})
+var todoMaker = function(text) {
+    var todo = document.createElement('li')
+    todo.textContent = text
+    todoList.appendChild(todo)
+}
+
+button.addEventListener('click' , function() {
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild)
+    }
+})
