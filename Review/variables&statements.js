17c16daf48b219983708cd317dc32d1b4cbe51b5
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-05-15 22:52:19 -0500

    variables and statements review

---
+++ Review/variables&statements.js
@@ -0,0 +1,24 @@
+// Declaring a variable
+
+var x = 'hey';
+y = 'hi';
+console.log(x)
+// hey
+
+let cool = true;
+cool = false;
+console.log(cool)
+// false
+
+const age = 10;
+//   age = 11; wrong: throws error
+// var and let can be updated but not const
+
+// Scoping:
+
+// var
+//  function scoped (only available inside parent functions)
+
+// let and const  
+//  block scoped (available inside a block denoted by { } )
+
