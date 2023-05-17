71e855abb9d8afb747c6e25cf55e54c51a94aa45
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-05-16 21:30:46 -0500

    string practice

---
+++ Review/reviewString.js
@@ -0,0 +1,24 @@
+// 3 ways to create strings:
+
+// using single quotes:
+
+const first = 'Bilal';
+
+// using double quotes:
+
+const middle = "-";
+
+// using backticks:
+const last = `Shahzad`;
+
+// single quotes and double quotes are the same thing
+
+// used for: "He's cool"
+
+// or escaping: 'he\\'s cool'
+
+// backticks:
+
+const sentence = `He's so "cool"`;
+console.log(sentence); // He's so "cool"
+
