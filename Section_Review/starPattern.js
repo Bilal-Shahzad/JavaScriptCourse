657c2b35281ca515d157f16c0685306d7e331683
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-06-07 23:23:09 -0500

    star pattern neted loop

---
+++ Section_Review/starPattern.js
@@ -0,0 +1,15 @@
+// nested loop star pattern 
+
+
+function showPattern(totalRowsPatternCount) {
+  for (let curRow = 1; curRow <= totalRowsPatternCount; curRow++) {
+    // console.log(curRow);
+    let patternDesign = '';
+    for (let i = 0; i < curRow; i++) {
+      patternDesign += '*'
+    }
+    console.log(patternDesign);
+  }
+}
+
+showPattern(5);
