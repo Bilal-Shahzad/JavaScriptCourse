8d5243eda2a53fe895ad7838f2e5f13da169e98d
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-06-21 18:57:33 -0500

    array from argument range

---
+++ Review/arrayArgRange.js
@@ -0,0 +1,19 @@
+// Create an array of the values from the 'min' and 'max' (start & end) range provided
+
+console.log('17-create-array-from-argument-range');
+
+function generateArrayFromRange(startNum, endNum) {
+  const rangeArray = [];
+  for(let curNum = startNum; curNum <= endNum; curNum++) {
+    // console.log('curNum: ', curNum);
+    rangeArray.push(curNum);
+    // console.log('rangeArray: ', rangeArray);
+  }
+  return rangeArray;
+}
+
+const range1 = generateArrayFromRange(1, 5);
+console.log(range1);
+console.log('----------');
+const range2 = generateArrayFromRange(-5, 0);
+console.log(range2);
